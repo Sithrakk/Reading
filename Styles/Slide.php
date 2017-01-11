de29bfd55ba88ae98233d40d90ca99d782344b04
Sithrakk <wanted15000@gmail.com>
2017-01-11 13:54:39 +0200

    Adding styling

---
+++ Styles/Slide.php
@@ -0,0 +1,65 @@
+<?php
+
+$title = "No it's not";
+$content = "<link rel='stylesheet' type='text/css' href='Styles/Gallery.css' />
+
+
+<h2 style='text-align:center'>Responsive Image Gallery</h2>
+
+<div class='responsive'>
+  <div class='img'>
+    <a target='_blank' href='http://z.mfcdn.net/store/manga/16627/031.0/compressed/c011.jpg'>
+      <img src='http://z.mfcdn.net/store/manga/16627/031.0/compressed/c011.jpg' alt='Trolltunga Norway' width='300' height='200'>
+    </a>
+  </div>
+</div>
+
+
+<div class='responsive'>
+  <div class='img'>
+    <a target='_blank' href='https://lh3.googleusercontent.com/-QB6vrTnhY7M/VcgHz04b0SI/AAAAAAABRS4/HwrDJIPkRfI/w850-h1227/Food%2BWars%2BShokugeki%2BNo%2BSouma%2BErina%2Band%2BAlice.jpg'>
+      <img src='https://lh3.googleusercontent.com/-QB6vrTnhY7M/VcgHz04b0SI/AAAAAAABRS4/HwrDJIPkRfI/w850-h1227/Food%2BWars%2BShokugeki%2BNo%2BSouma%2BErina%2Band%2BAlice.jpg' alt='Trolltunga Norway' width='300' height='200'>
+    </a>
+  </div>
+</div>
+
+
+<div class='responsive'>
+  <div class='img'>
+    <a target='_blank' href='http://vignette3.wikia.nocookie.net/highschooldxd/images/b/b3/Rias_Akeno_New_poster.jpg/revision/latest/scale-to-width-down/1280?cb=20131003235101'>
+      <img src='http://vignette3.wikia.nocookie.net/highschooldxd/images/b/b3/Rias_Akeno_New_poster.jpg/revision/latest/scale-to-width-down/1280?cb=2013100323510'1 alt='Trolltunga Norway' width='300' height='200'>
+    </a>
+  </div>
+</div>
+
+
+<div class='responsive'>
+  <div class='img'>
+    <a target='_blank' href='Mangas/anime-ecchi-cooking-art-667574.jpeg'>
+      <img src='Mangas/anime-ecchi-cooking-art-667574.jpeg' alt='Trolltunga Norway' width='300' height='200'>
+    </a>
+  </div>
+</div>
+
+
+<div class='responsive'>
+  <div class='img'>
+    <a target='_blank' href='Mangas/Anime-Blazblue-Noel-Vermillion-Headphones-800x960.jpg'>
+      <img src='Mangas/Anime-Blazblue-Noel-Vermillion-Headphones-800x960.jpg' alt='Trolltunga Norway' width='300' height='200'>
+    </a>
+  </div>
+</div>
+
+
+<div class='clearfix'></div>
+
+
+<div style='padding:6px;'>
+  <p>This example use media queries to re-arrange the images on different screen sizes: for screens larger than 700px wide, it will show four images side by side, for screens smaller than 700px, it will show two images side by side. For screens smaller than 500px, the images will stack vertically (100%).</p>
+  <p>You will learn more about media queries and responsive web design later in our CSS Tutorial.</p>
+  <h4>Resize the browser window to see the effect.</h4>
+</div>";
+
+
+require ('Template.php');
+?>
