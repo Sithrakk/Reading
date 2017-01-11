9be77a166b820e90e67b64dd7ff511f0610bcced
Sithrakk <wanted15000@gmail.com>
2017-01-11 10:58:15 +0200

    asfsgdf|

---
+++ Template.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo $title; ?></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" type="text/css" href="Styles/Stylesheet.css" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+        <script src="Styles/Bootstrap/js/bootstrap.min.js"></script>
+
+        
+    </head>
+
+    <body>
+        <div id="wrapper">
+            <nav id="navigation">
+                <ul id="nav">
+                            <li><a href="../Reading/Novels.php">Novels</a></li>
+                            <li><a href="../Reading/Mangas.php">Manga</a></li>
+                                    
+                            <li><a href="../Reading/TheThing.php">Well..</a></li> 
+                            <li><a href="../Reading/Slide.php">We'll..</a></li>
+                            <li><a href="">..Figure It Out</a></li>
+                    
+                </ul>
+            </nav>
+            
+                <div id="content_area">
+
+                    Say hello to ma lil firend.
+                </div>
+            
+            <footer>
+                <h4><p>Me, Myself & I, Thankme!</p></h4>
+            </footer>
+        </div>
+    </body>
+</html>
